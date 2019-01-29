7a383c14f80cae277c0c595a304598677b0ea748
Daniel DeLayo <dandelayo@gmail.com>
2019-01-29 12:40:51 -0500

    Left like, half the code in the jsfiddle. Whoops

--- content.js
+++ content.js
@@ -1,4 +1,54 @@
 http://jsfiddle.net/mvea1mmw/18/();
+function dateify(sDate) {
+  var dates = sDate.split("/");
+  return dates[2] + dates[0] + dates[1];
+}
+
+function untilDate(sDate) {
+  var dates = sDate.split("/");
+  return dates[2] + dates[0] + (parseInt(dates[1]) - 10);
+}
+
+function timeify(sTime) {
+  if (sTime.includes("PM")) {
+    sTime = sTime.replace("PM", "");
+    if (sTime < 1200) sTime = parseInt(sTime) + 1200;
+  } else {
+    sTime = sTime.replace("AM", "");
+    if (parseInt(sTime) < 1000) {
+      sTime = "0" + sTime;
+    }
+  }
+  sTime += "00";
+  return sTime;
+}
+
+function locationFormat(sLoc) {
+  var temp = sLoc.replace("LBR", "Library").replace("WESTCAMPUS", "").replace("COMPUTER SCI", "Old Computer Science").replace("HLL", "Hall").split(" ");
+  var s = "";
+  for (var i = 0; i < temp.length; i++) {
+    s += temp[i].charAt(0).toUpperCase();
+    for (var j = 1; j < temp[i].length; j++)
+      s += temp[i].charAt(j).toLowerCase();
+    if (i < temp.length - 1)
+      s += " ";
+  }
+  return s;
+}
+
+
+//var rows = document.getElementsByTagName("table")[0].rows; 
+//above for JS Fiddle
+//below for Solar
+var rows = frames[0].document.getElementById("ACE_STDNT_ENRL_SSV2$0").rows;
+
+var eventString = "BEGIN:VCALENDAR\nPRODID:-//Google Inc//Google Calendar 70.9054//EN\nVERSION:2.0\nCALSCALE:GREGORIAN\nMETHOD:PUBLISH\nX-WR-CALNAME:Schedule\nX-WR-TIMEZONE:America/New_York\nX-WR-CALDESC:My Schedule\n";
+for (var i = 1; i < rows.length; i += 2) {
+  var table = rows[i].getElementsByTagName("table")[0];
+  var tablePre = table.rows[1].getElementsByTagName("table")[0].rows[2].getElementsByTagName("table")[0];
+  for (var k = 1; k < tablePre.rows.length; k++) {
+    var infoTable = tablePre.rows[k].getElementsByTagName("td");
+    var nameInfo = table.rows[0].getElementsByTagName("table")[0].getElementsByTagName("table")[0].rows[0].innerHTML.trim();
     var className = nameInfo.substring(nameInfo.indexOf(">") + 1).split(" - ")[0];
 
     if (infoTable[2].getElementsByTagName("span")[0].innerHTML.trim().includes("Recitation"))
@@ -7,6 +57,7 @@ http://jsfiddle.net/mvea1mmw/18/();
       className += " Lab";
     var dateInfo = infoTable[6].getElementsByTagName("span")[0].innerHTML.trim();
     var date = dateify(dateInfo.split(" - ")[0].trim());
+    //alert("date: " + date);
     var desc = infoTable[3].getElementsByTagName("span")[0].innerHTML.trim().split(" - ");
     var time = desc[0].replace(":", "");
     time = time.substring(time.length - 6).trim();
@@ -18,6 +69,7 @@ http://jsfiddle.net/mvea1mmw/18/();
     var uString = untilDate(dateInfo.split(" - ")[1].trim()) + "T010000";
     var days = desc[0].substring(0, desc[0].length - 7).trim();
     var dayString = "";
+    var onMonday = false;
     for (var j = 0; j < days.length - 1; j += 2) {
       var day = days.substring(j, j + 2);
       if (day.includes("Th"))
@@ -26,8 +78,10 @@ http://jsfiddle.net/mvea1mmw/18/();
         dayString += "TU";
       else if (day.includes("We"))
         dayString += "WE";
-      else if (day.includes("Mo"))
+      else if (day.includes("Mo")) {
         dayString += "MO";
+        onMonday = true;
+      }
       else if (day.includes("Fr"))
         dayString += "FR";
       else if (day.includes("Sa"))
@@ -42,7 +96,12 @@ http://jsfiddle.net/mvea1mmw/18/();
     locString = locationFormat(locString);
     var stamp = new Date().toJSON().slice(0, 10).replace(/-/g, '');
     var profName = infoTable[5].getElementsByTagName("span")[0].innerHTML.trim()
-    eventString += "BEGIN:VEVENT\nCLASS:PUBLIC\nDTSTART;TZID=America/New_York:" + dString + "\nRRULE:FREQ=WEEKLY;UNTIL=" + uString + ";BYDAY=" + dayString + "\nDTEND;TZID=America/New_York:" + endTimeString + "\nEXDATE;TZID=America/New_York:" + dString + "\nDTSTAMP:" + stamp + "\nLOCATION:" + locString + "\nDESCRIPTION;LANGUAGE=en-us:" + profName + "\nTRANSP:TRANSPARENT\nSUMMARY:" + className.trim() + "\nEND:VEVENT\n";
+    eventString += "BEGIN:VEVENT\nCLASS:PUBLIC\nDTSTART;TZID=America/New_York:" + dString + "\nRRULE:FREQ=WEEKLY;UNTIL=" + uString + ";BYDAY=" + dayString + "\nDTEND;TZID=America/New_York:" + endTimeString;
+    if (!onMonday)
+    {
+    	eventString += "\nEXDATE;TZID=America/New_York:" + dString;
+    }
+    eventString += "\nDTSTAMP:" + stamp + "\nLOCATION:" + locString + "\nDESCRIPTION;LANGUAGE=en-us:" + profName + "\nTRANSP:TRANSPARENT\nSUMMARY:" + className.trim() + "\nEND:VEVENT\n";
   }
 }
 eventString += "END:VCALENDAR"
